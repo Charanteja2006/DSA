a2e9e954da95db4699baaeeda5cdfe3181ad4c81
Y Lakshmi Charan Teja <144612904+Charanteja2006@users.noreply.github.com>
2026-02-03 19:32:03 +0530

    Time: 25 ms (39.84%), Space: 78.5 MB (27.19%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> res(n,0);
+        for(auto it:bookings){
+            res[it[0]-1] += it[2];
+            if(it[1]<n) res[it[1]] += (-it[2]); 
+        }
+
+        for(int i=1;i<n;i++) res[i] += res[i-1];
+
+        return res;
+    }
+};
