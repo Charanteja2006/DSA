eb9f392dbadadee0a10f1268267efe0b2a637f47
Y Lakshmi Charan Teja <144612904+Charanteja2006@users.noreply.github.com>
2026-03-15 13:34:18 +0530

    Time: 905 ms (6.67%), Space: 333.1 MB (6.67%) - LeetHub

---
+++ 3868-minimum-cost-to-equalize-arrays-using-swaps/3868-minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minCost(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> mp1, mp2, mp;
+        int n = nums1.size();
+
+        for(int i = 0; i < n; i++){
+            mp1[nums1[i]]++;
+            mp2[nums2[i]]++;
+            mp[nums1[i]]++;
+            mp[nums2[i]]++;
+        }
+
+        for(auto &it : mp){
+            if(it.second % 2) return -1;
+        }
+
+        int cost = 0;
+
+        for(auto &it : mp){
+            int x = it.first;
+            cost += abs(mp1[x] - mp2[x]) / 2;
+        }
+
+        return cost / 2;
+    }
+};
