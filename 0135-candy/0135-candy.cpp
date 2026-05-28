e9d706afe6f5797edc2e372cfbfe0fd1d5eba680
Y Lakshmi Charan Teja <144612904+Charanteja2006@users.noreply.github.com>
2026-05-28 19:43:33 +0530

    Time: 0 ms (100%), Space: 22.2 MB (84.71%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,20 +3,30 @@ public:
     int candy(vector<int>& ratings) {
         
         int n = ratings.size();
-        vector<int> left(n,1),right(n,1);
-        int tot = 0;
 
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]) left[i] = left[i-1] + 1;
+        int tot = 1;
+        int i = 1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                i++;
+                tot += 1;
+                continue;
+            }
+            int peak = 1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak += 1;
+                tot += peak;
+                i++;
+            }
+            int down = 1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                tot += down;
+                down += 1;
+                i++;
+            }
+            if(down>peak) tot += down-peak;
         }
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]) right[i] = right[i+1] + 1;
-        }
-
-        for(int i=0;i<n;i++){
-            tot += max(left[i],right[i]);
-        }
-
         return tot;
+        
     }
 };
