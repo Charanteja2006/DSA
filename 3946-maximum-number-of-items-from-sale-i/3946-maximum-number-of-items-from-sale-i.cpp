c8f49657fe4497fd4569945b40174edbd00c329a
Y Lakshmi Charan Teja <144612904+Charanteja2006@users.noreply.github.com>
2026-05-31 19:43:34 +0530

    Time: 95 ms (77.29%), Space: 35.7 MB (81.01%) - LeetHub

---
+++ 3946-maximum-number-of-items-from-sale-i/3946-maximum-number-of-items-from-sale-i.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int maximumSaleItems(vector<vector<int>>& items, int budget) {
+        int n = items.size();
+
+        vector<int> value(n);
+        int minPrice = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            int freebies = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j && items[j][0] % items[i][0] == 0)
+                    freebies++;
+            }
+
+            value[i] = 1 + freebies;   
+            minPrice = min(minPrice, items[i][1]);
+        }
+
+        vector<int> dp(budget + 1, -1e9);
+        dp[0] = 0;
+
+       
+        for (int i = 0; i < n; i++) {
+            int cost = items[i][1];
+
+            for (int b = budget; b >= cost; b--) {
+                dp[b] = max(dp[b], dp[b - cost] + value[i]);
+            }
+        }
+
+        int ans = 0;
+
+        for (int spent = 0; spent <= budget; spent++) {
+            
+
+            int remaining = budget - spent;
+
+            ans = max(ans,
+                      dp[spent] + remaining / minPrice);
+        }
+
+        return ans;
+    }
+};
