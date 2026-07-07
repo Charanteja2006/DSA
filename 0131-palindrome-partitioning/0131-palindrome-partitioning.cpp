468f02210ef3fd6e911da41c9f0e4548f590143c
Y Lakshmi Charan Teja <144612904+Charanteja2006@users.noreply.github.com>
2026-07-07 12:15:46 +0530

    Time: 60 ms (32.07%), Space: 79.9 MB (24.28%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    bool pal(int i,int j,string s){
-        while(i<j){
-            if(s[i]!=s[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
+    vector<vector<string>> res;
+    vector<string> cur;
 
-    void find(int i,int j,vector<vector<string>> &res,vector<string>& cur ,string s){
-        int n = s.size();
-        if(j==n-1){
-            if(pal(i,j,s)){
-                cur.push_back(s.substr(i,j-i+1));
-                res.push_back(cur);
-                cur.pop_back();
-            }    
+    void backtrack(int start, const string& s, vector<vector<bool>>& dp) {
+        if (start == s.size()) {
+            res.push_back(cur);
             return;
         }
 
-        if(pal(i,j,s)){
-            cur.push_back(s.substr(i,j-i+1));
-            find(j+1,j+1,res,cur,s);
-            cur.pop_back();
-        }   
-
-        find(i,j+1,res,cur,s);
-        
+        for (int end = start; end < s.size(); end++) {
+            if (dp[start][end]) {
+                cur.push_back(s.substr(start, end - start + 1));
+                backtrack(end + 1, s, dp);
+                cur.pop_back();
+            }
+        }
     }
 
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string> cur;
-        find(0,0,res,cur,s);
+        int n = s.size();
+
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s[i] == s[j]) {
+                    if (j - i <= 2)
+                        dp[i][j] = true;
+                    else
+                        dp[i][j] = dp[i + 1][j - 1];
+                }
+            }
+        }
+
+        backtrack(0, s, dp);
         return res;
     }
 };
