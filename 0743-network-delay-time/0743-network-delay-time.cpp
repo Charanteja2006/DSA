487631b41447b767168ae557d195c53177dc350b
Y Lakshmi Charan Teja <144612904+Charanteja2006@users.noreply.github.com>
2026-07-14 12:48:30 +0530

    Time: 112 ms (5.08%), Space: 60.4 MB (5.09%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n+1);
+        vector<vector<vector<int>>> adj(n+1);
 
         for(auto it:times){
-            int u = it[0];
-            int v = it[1];
-            int tm = it[2];
-            adj[u].push_back({v,tm});
+            adj[it[0]].push_back({it[1],it[2]});
         }
 
         vector<int> dis(n+1,INT_MAX);
-        dis[k] = 0;
-        queue<pair<int,int>> q;
-        q.push({k,0});
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
+        q.push({0,k});  
+        dis[0] = dis[k] = 0;
 
         while(!q.empty()){
-            int node = q.front().first;
-            int time = q.front().second;
+            int node = q.top().second;
+            int time = q.top().first;
             q.pop();
-            if(time>dis[node]) continue;
+
+            if(dis[node]<time) continue;
 
             for(auto it:adj[node]){
-                if(dis[it.first]>time+it.second){
-                    dis[it.first] = time + it.second;
-                    q.push({it.first,dis[it.first]});
+                if(dis[it[0]] > time + it[1]){
+                    dis[it[0]] = time + it[1];
+                    q.push({dis[it[0]],it[0]});
                 }
             }
         }
 
-        int res = *max_element(dis.begin()+1,dis.end());
-        return res == INT_MAX? - 1: res;
+        int res = *max_element(dis.begin(),dis.end());
+        return res==INT_MAX ? -1 : res;
     }
 };
