1bc7fc1eae4b3e3a5600fdecf4f0a40d18093c97
Y Lakshmi Charan Teja <144612904+Charanteja2006@users.noreply.github.com>
2026-07-22 19:52:47 +0530

    Time: 563 ms (15.64%), Space: 296.5 MB (21.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+
+    int find(int i,int pre,vector<int> &nums,vector<vector<int>> &dp){
+        if(i>=nums.size()) return 0;
+        if(dp[i][pre+1]!=-1) return dp[i][pre+1];
+        int len = find(i+1,pre,nums,dp);
+        if(pre==-1||nums[i]>nums[pre]){
+            len = max(len,1+find(i+1,i,nums,dp));
+        }
+        return dp[i][pre+1]=len;
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,1);
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i] = max(dp[i], 1 + dp[j]);
-                } 
-            }
-        }
-
-        return *max_element(dp.begin(),dp.end());
+        vector<vector<int>> dp(n,vector<int> (n+1,-1));
+        return find(0,-1,nums,dp);
     }
 };
